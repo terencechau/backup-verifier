1c1fc0a1d7b6300c6a39072e0394d066eb22992d
Terence Chau <34471214+terencechau@users.noreply.github.com>
2025-08-09 10:16:16 -0500

    fixed example code

--- verify_backups_example.py
+++ verify_backups_example.py
@@ -1,9 +1,15 @@
-import backup_verifier_functions.py
+from backup_verifier_functions import *
 
-hashes_drive_1 = hash_folder("drive_1_path")
-save_hashes(hashes_drive_1, "output_hash_1_path.json")
+folder_1 = "folder_1_path"
+folder_2 = "folder_2_path"
 
-hashes_drive_2 = hash_folder("drive_2_path")
-save_hashes(hashes_drive_2, "output_hash_2_path.json")
+hash_1_path = "hash_1.json"
+hash_2_path = "hash_2.json"
 
-compare_hashes(hashes_drive_1, hashes_drive_2)
+hashes_folder_1 = hash_folder(folder_1)
+save_hashes(hashes_folder_1, hash_1_path)
+
+hashes_folder_2 = hash_folder(folder_2)
+save_hashes(hashes_folder_2, hash_2_path)
+
+compare_hashes(hashes_folder_1, hashes_folder_2)
